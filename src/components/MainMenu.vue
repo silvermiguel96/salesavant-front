66ac82b2a995b0b8007903835f06bddb66d89fe0
camilo <camilomontoya2@gmail.com>
2019-05-20 20:42:19 -0500

    router

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-toolbar color="primary" dark app dense>
+    <img class="logo" :src="require('../assets/salesavant_header.png')">
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data: () => {}
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 11%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
-  <HelloWorld />
+  <div class="about">
+    <h1>This is home page</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from "../components/HelloWorld";
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  components: {
+    HelloWorld
   }
+};
 </script>
