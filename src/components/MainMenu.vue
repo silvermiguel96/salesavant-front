60944af6fb56156468c621a9d68c71eb736385fd
camilo <camilomontoya2@gmail.com>
2019-05-21 08:52:52 -0500

    menu routerlinks

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,11 +1,11 @@
 <template>
   <v-toolbar color="primary" dark app dense>
-    <img class="logo" :src="require('../assets/salesavant_header.png')" />
+    <img class="logo" :src="require('../assets/salesavant_header.png')">
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Playlists</v-btn>
-      <v-btn flat>Search</v-btn>
-      <v-btn flat>Search with Signals</v-btn>
+      <v-btn flat to="/playlists">Playlists</v-btn>
+      <v-btn flat to="/search">Search</v-btn>
+      <v-btn flat to="/search-with-signals">Search with Signals</v-btn>
     </v-toolbar-items>
     <v-toolbar-side-icon></v-toolbar-side-icon>
   </v-toolbar>
