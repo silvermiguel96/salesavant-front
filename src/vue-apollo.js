68e39de5eeabc839dcdd559e16ef71b61ee3f7c8
camilo <camilomontoya2@gmail.com>
2019-05-21 08:08:34 -0500

    vue-apollo config

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -1,23 +1,24 @@
-import Vue from "vue";
-import VueApollo from "vue-apollo";
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import {
   createApolloClient,
   restartWebsockets
-} from "vue-cli-plugin-apollo/graphql-client";
+} from 'vue-cli-plugin-apollo/graphql-client';
 
 // Install the vue plugin
 Vue.use(VueApollo);
 
 // Name of the localStorage item
-const AUTH_TOKEN = "apollo-token";
+const AUTH_TOKEN = 'apollo-token';
 
 // Http endpoint
 const httpEndpoint =
-  process.env.VUE_APP_GRAPHQL_HTTP || "http://localhost:4000/graphql";
+  process.env.VUE_APP_GRAPHQL_HTTP ||
+  'http://ec2-18-212-70-8.compute-1.amazonaws.com:4000/graphql';
 // Files URL root
 export const filesRoot =
   process.env.VUE_APP_FILES_ROOT ||
-  httpEndpoint.substr(0, httpEndpoint.indexOf("/graphql"));
+  httpEndpoint.substr(0, httpEndpoint.indexOf('/graphql'));
 
 Vue.prototype.$filesRoot = filesRoot;
 
@@ -25,35 +26,12 @@ Vue.prototype.$filesRoot = filesRoot;
 const defaultOptions = {
   // You can use `https` for secure connection (recommended in production)
   httpEndpoint,
-  // You can use `wss` for secure connection (recommended in production)
-  // Use `null` to disable subscriptions
-  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || "ws://localhost:4000/graphql",
-  // LocalStorage token
+  wsEndpoint: null,
   tokenName: AUTH_TOKEN,
-  // Enable Automatic Query persisting with Apollo Engine
   persisting: false,
-  // Use websockets for everything (no HTTP)
-  // You need to pass a `wsEndpoint` for this to work
   websocketsOnly: false,
-  // Is being rendered on the server?
-  ssr: false
-
-  // Override default apollo link
-  // note: don't override httpLink here, specify httpLink options in the
-  // httpLinkOptions property of defaultOptions.
-  // link: myLink
-
-  // Override default cache
-  // cache: myCache
-
-  // Override the way the Authorization header is set
-  // getAuth: (tokenName) => ...
-
-  // Additional ApolloClient options
-  // apollo: { ... }
-
-  // Client local data (see apollo-link-state)
-  // clientState: { resolvers: { ... }, defaults: { ... } }
+  ssr: false,
+  getAuth: () => `Basic YWxlamFuZHJvQHNhbGVzYXZhbnQuY29tOnF3ZXF3ZQ==`
 };
 
 // Call this in the Vue app file
@@ -76,8 +54,8 @@ export function createProvider(options = {}) {
     errorHandler(error) {
       // eslint-disable-next-line no-console
       console.log(
-        "%cError",
-        "background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;",
+        '%cError',
+        'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;',
         error.message
       );
     }
@@ -88,7 +66,7 @@ export function createProvider(options = {}) {
 
 // Manually call this when user log in
 export async function onLogin(apolloClient, token) {
-  if (typeof localStorage !== "undefined" && token) {
+  if (typeof localStorage !== 'undefined' && token) {
     localStorage.setItem(AUTH_TOKEN, token);
   }
   if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient);
@@ -96,13 +74,13 @@ export async function onLogin(apolloClient, token) {
     await apolloClient.resetStore();
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.log("%cError on cache reset (login)", "color: orange;", e.message);
+    console.log('%cError on cache reset (login)', 'color: orange;', e.message);
   }
 }
 
 // Manually call this when user log out
 export async function onLogout(apolloClient) {
-  if (typeof localStorage !== "undefined") {
+  if (typeof localStorage !== 'undefined') {
     localStorage.removeItem(AUTH_TOKEN);
   }
   if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient);
@@ -110,6 +88,6 @@ export async function onLogout(apolloClient) {
     await apolloClient.resetStore();
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.log("%cError on cache reset (logout)", "color: orange;", e.message);
+    console.log('%cError on cache reset (logout)', 'color: orange;', e.message);
   }
 }
