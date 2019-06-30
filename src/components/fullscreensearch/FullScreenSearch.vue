9f973da4ad533c54219d2e9dc8db6363607d372e
camilo <camilomontoya2@gmail.com>
2019-06-30 08:38:01 -0500

    remove triggering button

--- src/components/fullscreensearch/FullScreenSearch.vue
+++ src/components/fullscreensearch/FullScreenSearch.vue
@@ -1,9 +1,6 @@
 <template>
   <v-layout row justify-center>
     <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-      </template>
       <v-card>
         <v-toolbar dark color="primary">
           <v-btn icon dark @click="dialog = false">
