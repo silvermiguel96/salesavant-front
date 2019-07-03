3df2c706b86c932526b9180bbf48c69426a1607e
camilo montoya <camilomontoya2@gmail.com>
2019-07-03 11:40:37 -0500

    Create playlists from search (#19)
    * SAVE PLAYLIST BUTTON
    
    * save simple search as a playlist
    
    * create playlist from company search

--- src/components/fullscreensearch/CompaniesSearch.vue
+++ src/components/fullscreensearch/CompaniesSearch.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-subheader>Companies Search</v-subheader>
+    <v-alert :value="showError" dismissible type="error">{{errorMessage}}</v-alert>
     <v-form ref="simpleSearchForm" @submit.prevent>
       <v-container grid-list-xs text-xs-center>
         <v-layout row wrap>
@@ -53,12 +54,27 @@
               @input="changeData"
             ></v-text-field>
           </v-flex>
-        </v-layout>
-        <v-layout row wrap>
-          <v-flex xs3>
+          <v-flex xs4 />
+          <v-flex xs4>
             <v-btn @click="search" type="submit" color="primary">
               <v-icon>search</v-icon>
               {{" Do a companies Search"}}
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex xs8>
+            <v-text-field
+              v-if="showNewPlaylistName"
+              name="newPlaylistName"
+              v-model="newPlaylistName"
+              label="New Playlist Name"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs4>
+            <v-btn @click="save" color="success">
+              <v-icon>save</v-icon>
+              {{" Save Companies search results as playlist"}}
             </v-btn>
           </v-flex>
         </v-layout>
@@ -68,6 +84,9 @@
 </template>
 
 <script>
+import { setTimeout } from "timers";
+import gql from "graphql-tag";
+import _get from "lodash.get";
 export default {
   data() {
     return {
@@ -79,9 +98,13 @@ export default {
         region: "",
         state: "",
         status: "",
-        lessThanEmployees: "",
-        moreThanEmployees: ""
-      }
+        lessThanEmployees: 0,
+        moreThanEmployees: 0
+      },
+      showError: false,
+      newPlaylistName: "",
+      errorMessage: "",
+      showNewPlaylistName: false
     };
   },
   methods: {
@@ -94,6 +117,111 @@ export default {
     search() {
       this.toggle();
       this.$emit("search");
+    },
+    async save() {
+      if (!this.atLeastOneCompanyFieldIsNotEmpty()) {
+        this.errorMessage =
+          "At least one company field search criteria is not empty!";
+        this.showError = true;
+        setTimeout(() => {
+          this.showError = false;
+        }, 5000);
+        return;
+      }
+      if (!this.showNewPlaylistName) {
+        this.showNewPlaylistName = true;
+        return;
+      }
+      if (!this.newPlaylistName) {
+        this.errorMessage = "New Playlist Name can not be empty!";
+        this.showError = true;
+        setTimeout(() => {
+          this.showError = false;
+        }, 5000);
+        return;
+      }
+      if (this.atLeastOneCompanyFieldIsNotEmpty() && !!this.newPlaylistName) {
+        try {
+          const result = await this.$apollo.mutate({
+            mutation: gql`
+              mutation(
+                $name: String
+                $city: String
+                $state: String
+                $region: String
+                $country: String
+                $status: String
+                $lessThanEmployees: Int
+                $moreThanEmployees: Int
+                $playlistName: String!
+              ) {
+                createPlaylistFromSearch(
+                  companySearch: {
+                    name: $name
+                    city: $city
+                    state: $state
+                    region: $region
+                    country: $country
+                    status: $status
+                    lessThanEmployees: $lessThanEmployees
+                    moreThanEmployees: $moreThanEmployees
+                  }
+                  playlistData: { name: $playlistName }
+                ) {
+                  playlist {
+                    uid
+                    name
+                  }
+                }
+              }
+            `,
+            // Parameters
+            variables: {
+              name: _get(this.company, "name", ""),
+              country: _get(this.company, "country", ""),
+              website: _get(this.company, "website", ""),
+              city: _get(this.company, "city", ""),
+              region: _get(this.company, "region", ""),
+              state: _get(this.company, "state", ""),
+              status: _get(this.company, "status", ""),
+              lessThanEmployees: _get(this.company, "lessThanEmployees", "0"),
+              moreThanEmployees: _get(this.company, "moreThanEmployees", "0"),
+              playlistName: this.newPlaylistName
+            }
+          });
+          console.log("saving simple search success", result);
+          const playlist = _get(
+            result,
+            "data.createPlaylistFromSearch.playlist",
+            null
+          );
+          if (!playlist) {
+            this.errorMessage =
+              "it seems that we created your playlist but couldn't check it, please check manually";
+            this.showError = true;
+            return;
+          }
+          this.toggle();
+          this.$router.push({
+            path: `/playlists/${playlist.uid}/companies`
+          });
+        } catch (error) {
+          this.errorMessage = "oops we did something wrong!";
+          this.showError = true;
+          console.log("error saving simple search as a play list", error);
+        }
+      }
+    },
+    atLeastOneCompanyFieldIsNotEmpty() {
+      let result = false;
+      for (let key in this.company) {
+        console.log("key", key);
+        if (!!this.company[key]) {
+          result = true;
+          break;
+        }
+      }
+      return result;
     }
   }
 };
--- src/components/fullscreensearch/SimpleSearch.vue
+++ src/components/fullscreensearch/SimpleSearch.vue
@@ -1,40 +1,58 @@
 <template>
-  <v-list three-line subheader>
+  <div>
     <v-subheader>Simple Search</v-subheader>
-    <v-list-tile avatar>
-      <v-list-tile-content>
-        <v-list-tile-sub-title>
-          <v-form ref="simpleSearchForm" @submit.prevent>
-            <v-container grid-list-md text-xs-center>
-              <v-layout row wrap>
-                <v-flex>
-                  <v-text-field
-                    name="simpleSearch"
-                    v-model="simpleSearch"
-                    label="name or description"
-                    @input="changeData"
-                  ></v-text-field>
-                </v-flex>
-                <v-flex>
-                  <v-btn @click="search" type="submit" color="primary">
-                    <v-icon>search</v-icon>
-                    {{" Do a simple Search"}}
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-form>
-        </v-list-tile-sub-title>
-      </v-list-tile-content>
-    </v-list-tile>
-  </v-list>
+
+    <v-alert :value="showError" dismissible type="error">{{errorMessage}}</v-alert>
+
+    <v-form ref="simpleSearchForm" @submit.prevent>
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex>
+            <v-text-field
+              name="simpleSearch"
+              v-model="simpleSearch"
+              label="name or description"
+              @input="changeData"
+            ></v-text-field>
+          </v-flex>
+          <v-flex v-if="showNewPlaylistName">
+            <v-text-field
+              name="newPlaylistName"
+              v-model="newPlaylistName"
+              label="New Playlist Name"
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex>
+            <v-btn @click="search" type="submit" color="primary">
+              <v-icon>search</v-icon>
+              {{" Do a simple Search"}}
+            </v-btn>
+          </v-flex>
+          <v-flex>
+            <v-btn @click="save" color="success">
+              <v-icon>save</v-icon>
+              {{" Save search results as playlist"}}
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
 <script>
+import gql from "graphql-tag";
+import _get from "lodash.get";
 export default {
   data() {
     return {
-      simpleSearch: ""
+      simpleSearch: "",
+      showError: false,
+      newPlaylistName: "",
+      errorMessage: "",
+      showNewPlaylistName: false
     };
   },
   methods: {
@@ -47,6 +65,66 @@ export default {
     search() {
       this.toggle();
       this.$emit("search");
+    },
+    async save() {
+      if (!this.simpleSearch) {
+        this.errorMessage = "Simple search criteria can not be empty!";
+        this.showError = true;
+        return;
+      }
+      if (!this.showNewPlaylistName) {
+        this.showNewPlaylistName = true;
+        return;
+      }
+      if (!this.newPlaylistName) {
+        this.errorMessage = "New Playlist Name can not be empty!";
+        this.showError = true;
+        return;
+      }
+      if (!!this.simpleSearch && !!this.newPlaylistName) {
+        try {
+          const result = await this.$apollo.mutate({
+            mutation: gql`
+              mutation($simpleSearch: String, $playlistName: String!) {
+                createPlaylistFromSearch(
+                  companySearch: { search: $simpleSearch }
+                  playlistData: { name: $playlistName }
+                ) {
+                  playlist {
+                    uid
+                    name
+                  }
+                }
+              }
+            `,
+            // Parameters
+            variables: {
+              simpleSearch: this.simpleSearch,
+              playlistName: this.newPlaylistName
+            }
+          });
+          console.log("saving simple search success", result);
+          const playlist = _get(
+            result,
+            "data.createPlaylistFromSearch.playlist",
+            null
+          );
+          if (!playlist) {
+            this.errorMessage =
+              "it seems that we created your playlist but couldn't check it, please check manually";
+            this.showError = true;
+            return;
+          }
+          this.toggle();
+          this.$router.push({
+            path: `/playlists/${playlist.uid}/companies`
+          });
+        } catch (error) {
+          this.errorMessage = "oops we did something wrong!";
+          this.showError = true;
+          console.log("error saving simple search as a play list", error);
+        }
+      }
     }
   }
 };
