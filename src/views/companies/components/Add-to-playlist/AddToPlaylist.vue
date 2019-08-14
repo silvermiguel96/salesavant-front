cd3df23fc9476a76eecacf0ec0d58c879997b8d9
silvermiguel96 <silvermiguel96@gmail.com>
2019-08-14 15:32:45 -0500

    Fixed in playlists

--- src/views/companies/components/Add-to-playlist/AddToPlaylist.vue
+++ src/views/companies/components/Add-to-playlist/AddToPlaylist.vue
@@ -1,8 +1,28 @@
 <template>
   <v-card class="ma-3">
+    <v-snackbar top v-model="snack" :timeout="10000" :color="snackColor">
+      {{ snackText }}
+      <v-btn flat @click="snack = false">Close</v-btn>
+    </v-snackbar>
     <v-card-title>
       <h1 class="display-1">Playlists</h1>
     </v-card-title>
+    <v-card-text>
+      <v-layout row>
+        <v-flex md9 lg10>
+          <playlists-autocomplete
+            :placeholder="'Add signal'"
+            @change="onPlaylistAutoCompleteChange"
+            @onSearch="onSignalAutoCompleteSearch"
+          />
+        </v-flex>
+        <v-flex sm3 lg2>
+          <v-btn @click="addPlaylistToCompany" :disabled="!playlistUid && !currentPlaylistSearch">
+            <v-icon left>add</v-icon>Add
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
     <v-card-text>
       <v-data-table
         :headers="headers"
@@ -11,13 +31,15 @@
         :pagination.sync="pagination"
         :rows-per-page-items="pagination.rowsPerPageItems"
         :total-items="totalItems"
-        @update:pagination="updatePagination"
+        @updatepagination="updatePagination"
       >
         <template v-slot:items="props">
           <td>
-          <router-link :to="`/playlists/${props.item.uid}/companies`" >{{ props.item.name || "--"}}</router-link>
+            <router-link :to="`/playlists/${props.item.uid}/companies`">{{ props.item.name || "--"}}</router-link>
           </td>
-          <td><v-icon>delete</v-icon></td>
+          <td>
+            <v-icon>delete</v-icon>
+          </td>
         </template>
       </v-data-table>
     </v-card-text>
@@ -26,9 +48,19 @@
 
 <script>
 import gql from "graphql-tag";
+import playlistsAutocomplete from "../../../../components/playlists/Autocomplete.vue";
+import _get from "lodash.get";
 export default {
   data() {
     return {
+      playlistUid: null,
+      playlistsList: [],
+      companyPlaylists: [],
+      currentPlaylistSearch: null,
+      snack: false,
+      snackColor: "",
+      snackText: "",
+      updatePagination: "",
       pagination: {
         page: 1,
         rowsPerPage: 25,
@@ -43,6 +75,9 @@ export default {
         { text: "Action", value: "action" }
       ]
     };
+  },
+  components: {
+    playlistsAutocomplete
   },
   methods: {
     updatePagination({
@@ -92,6 +127,80 @@ export default {
         };
       },
       fetchPolicy: "cache-and-network"
+    }
+  },
+  methods: {
+    onPlaylistAutoCompleteChange(playlistResults) {
+      this.playlistUid = _get(playlistResults, "playlistUid", null);
+      this.currentPlaylistSearch = _get(
+        playlistResults,
+        "currentPlaylistSearch",
+        null
+      );
+    },
+    onSignalAutoCompleteSearch(playlistResults) {
+      this.currentPlaylistSearch = _get(
+        playlistResults,
+        "currentPlaylistSearch",
+        null
+      );
+    },
+    async addPlaylistToCompany() {
+      console.log("Ingreso a la funcion");
+      try {
+        console.log("Ingreso a la promesa");
+        if (!!this.playlistUid || !!this.currentPlaylistSearch) {
+          console.log("hola3");
+          const result = await this.$apollo.mutate({
+            mutation: gql`
+                mutation( $companyUid: String!, $playlistUid: String!) {
+                  addCompanyToPlaylist( playlistUid: $playlistUid companyUid: $companyUid ) {
+                    playlist{
+                      uid
+                      name
+                      }
+                    }
+                  }
+              `,
+            // Parameters
+            variables: {
+              playlistUid: this.playlistUid,
+              companyUid: this.$route.params.companiesUid
+            }
+          });
+          console.log("result", result);
+          const  newPlaylistId = _get(
+            result,
+            "data.addCompanyToPlaylist.playlist.uid",
+            null
+          );
+          console.log("newPlaylistId", newPlaylistId);
+          if (!newPlaylistId) {
+            this.snack = true;
+            this.snackColor = "error";
+            this.snackText =
+              "Oops!! we did something wrong when saving the company - playlist, please try again!!";
+            return;
+          }
+            this.snack = true;
+            this.snackColor = "success";
+            this.snackText =
+              "The playlist is add to company";
+            return;
+        } else {
+          console.log("hola5");
+          this.snack = true;
+          this.snackColor = "error";
+          this.snackText = "Select a playlist first";
+          return;
+        }
+      } catch (error) {
+        console.log("Error");
+        this.snack = true;
+        this.snackColor = "error";
+        this.snackText = "Oops we did something wrong!!";
+        console.log("error adding playlist to company", error);
+      }
     }
   },
   beforeUpdate() {
