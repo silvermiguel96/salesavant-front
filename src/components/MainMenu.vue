635255ab7cbad81508736a3b36bb8435d5ef5345
silvermiguel96 <silvermiguel96@gmail.com>
2019-09-09 11:23:07 -0500

    Main and Buttons

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -6,38 +6,38 @@
       </router-link>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down" v-for="link in links" :key="link.text">
-        <v-btn v-if="!!link.click" flat @click="link.click">{{link.text}}</v-btn>
+        <v-btn v-if="!!link.click" text @click="link.click">{{link.text}}</v-btn>
         <v-btn v-else flat :to="link.router">{{link.text}}</v-btn>
       </v-toolbar-items>
       <v-toolbar-items>
-        <v-btn flat color="white" @click="this.exitApp">
+        <v-btn text color="white" @click="this.exitApp">
           <span>Sign Out</span>
-          <v-icon right >exit_to_app</v-icon>
+          <v-icon right>exit_to_app</v-icon>
         </v-btn>
       </v-toolbar-items>
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-toolbar>
     <v-navigation-drawer right app v-model="drawer" class="primary">
-      <v-layout column align-center>
-        <v-flex class="mt-4">
-          <img class="text-lg-center" src="../../public/static/salesavant_header.png" />
-        </v-flex>
-      </v-layout>
+      <v-row align="center" justify="center">
+        <v-img :src="require('../assets/salesavant_header.png')" max-height="200" max-width="240" class="ma-2" alt="salesavant.png"></v-img>
+      </v-row>
       <v-list>
-        <v-list-tile v-for="link in links" :key="link.text" :to="link.route" @click="link.click">
-          <v-list-tile-action>
-            <v-icon class="white--text">{{ link.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="white--text" :to="link.router">{{ link.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+        <v-list-item-group>
+          <v-list-item v-for="link in links" :key="link.text" :to="link.route" @click="link.click">
+            <v-list-item-icon>
+              <v-icon class="white--text">{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="white--text" :to="link.router">{{ link.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
 <script>
-import { AUTH_TOKEN } from '../vue-apollo';
+import { AUTH_TOKEN } from "../vue-apollo";
 
 export default {
   data() {
@@ -50,8 +50,18 @@ export default {
           router: "/playlists",
           click: this.onPlaylists
         },
-        { icon: "business", text: "Companies", router: "/companies", click: this.onCompanies },
-        { icon: "assessment", text: "News", router: "/news", click: this.onNews },
+        {
+          icon: "business",
+          text: "Companies",
+          router: "/companies",
+          click: this.onCompanies
+        },
+        {
+          icon: "assessment",
+          text: "News",
+          router: "/news",
+          click: this.onNews
+        },
         { icon: "search", text: "Search", click: this.toggleSearch },
         { icon: "work", text: "Jobs Queue", click: this.toggleJobsQueuve }
       ]
@@ -78,11 +88,11 @@ export default {
       this.$router.push("/companies");
     },
     onNews() {
-      this.$router.push("/news")
+      this.$router.push("/news");
     },
     exitApp() {
       localStorage.removeItem(AUTH_TOKEN);
-       this.$router.go("/login")
+      this.$router.go("/login");
     }
   }
 };
