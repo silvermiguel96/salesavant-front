e949134c58670775b57859f5bffd41514eaa3d6a
silvermiguel96 <silvermiguel96@gmail.com>
2019-09-09 09:55:17 -0500

    Change dependencies and dev-dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import "./plugins/vuetify";
+import vuetify from "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import { createProvider } from "./vue-apollo";
@@ -7,6 +7,7 @@ import { createProvider } from "./vue-apollo";
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
   apolloProvider: createProvider(),
   render: h => h(App)
