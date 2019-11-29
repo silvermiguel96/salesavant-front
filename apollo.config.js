9e225e7558e35068ca568a0cbd3265c2a4f05bdb
Alejandro <lariverosc@gmail.com>
2019-11-29 01:02:40 -0500

    fix conf

--- apollo.config.js
+++ apollo.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-    client: {
-        service: {
-            name: 'my-app',
-            url: 'http://localhost:4000/api',
-        },
-        includes: [
-            'src/**/*.vue',
-            'src/**/*.js'
-        ]
-    }
-}
+  client: {
+    service: {
+      name: "my-app",
+      url: "http://localhost:4000/api"
+    },
+    includes: ["src/**/*.vue", "src/**/*.js"]
+  }
+};
