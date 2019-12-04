d0bc204ac43d4c5894c9977437b495c56e9c4c23
Miguelangel <silvermiguel96@gmail.com>
2019-12-04 17:41:47 -0500

    long paragraph and pagination 25

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
-module.exports = function() {
+module.exports = function(api) {
+  api.cache.forever();
   if (process.env.NODE_ENV === "production" || process.env.BABEL_ENV === "production") {
     return {
       presets: ["@vue/app"],
