1044376ba2c124b37305ad7d2d96b756b2cf3f17
Alejandro <lariverosc@gmail.com>
2019-12-27 17:09:21 -0500

    wip

--- src/components/upload/Upload.vue
+++ src/components/upload/Upload.vue
@@ -22,10 +22,14 @@
                 <v-text-field label="Description"></v-text-field>
               </v-col>
               <v-col cols="12">
-                <v-file-input label="Document upload"></v-file-input>
+                <v-file-input
+                  v-model="file"
+                  accept=".csv"
+                  label="Document upload"
+                ></v-file-input>
               </v-col>
               <v-col cols="12">
-                <v-btn color="primary" class="text-capitalize" block>Save</v-btn>
+                <v-btn color="primary" class="text-capitalize" @click.prevent="submitFiles">Save</v-btn>
               </v-col>
             </v-row>
           </v-container>
@@ -39,9 +43,33 @@
 export default {
   data() {
     return {
-      items: ["ORB refresh", "Contacts", "Keywords"]
+      items: ["ORB refresh", "Contacts", "Keywords"],
+      file: null,
     };
   },
-  components: {}
+  props: {
+    salesavantAPI: { type: String, default: process.env.VUE_APP_REST_API_URL }
+  },
+  components: {},
+  methods: {
+    submitFiles() {
+      if (this.file) {
+        let formData = new FormData();
+        formData.append("files", this.file, this.file.name);
+        fetch(this.salesavantAPI + "/files/upload", {
+          method: "POST",
+          body: formData
+        })
+          .then(response => {
+            console.log(response.json());
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      } else {
+        console.log("there are no files.");
+      }
+    }
+  }
 };
 </script>
--- src/views/batch/components/BatchJobRow.vue
+++ src/views/batch/components/BatchJobRow.vue
@@ -1,40 +1,41 @@
 <template >
-      <tr>
-        <td>
-          <div>
-              <router-link :to="`/playlists/${additionalDataParsed.playlist_uid}/companies`">{{ additionalDataParsed.playlist_name }}</router-link> 
-          </div>  
-          <div class="font-weight-light"> {{ getJobName(job.jobType) }}</div>  
-        </td>
-        <td>{{ job.description || "--" }}</td>
-        <td>{{ changeTimeHuman(job.creationTime) }}</td>
-        <td v-if="job.progress > 0 && job.progress < 100">
-          <v-progress-circular
-            :rotate="-90"
-            :size="38"
-            :width="4"
-            color="light-blue"
-            :value="job.progress.toFixed(0)"
-            style="font-size: 0.9em;"
-          >{{ job.progress.toFixed(0) }}%</v-progress-circular>
-        </td>
-        <td v-else-if="job.progress >= 100">100%</td>
-        <td v-else>Waiting...</td>
+  <tr>
+    <td>
+      <div>
+        <router-link
+          :to="`/playlists/${additionalDataParsed.playlist_uid}/companies`"
+        >{{ additionalDataParsed.playlist_name }}</router-link>
+      </div>
+      <div class="font-weight-light">{{ getJobName(job.jobType) }}</div>
+    </td>
+    <td>{{ job.description || "--" }}</td>
+    <td>{{ changeTimeHuman(job.creationTime) }}</td>
+    <td v-if="job.progress > 0 && job.progress < 100">
+      <v-progress-circular
+        :rotate="-90"
+        :size="38"
+        :width="4"
+        color="light-blue"
+        :value="job.progress.toFixed(0)"
+        style="font-size: 0.9em;"
+      >{{ job.progress.toFixed(0) }}%</v-progress-circular>
+    </td>
+    <td v-else-if="job.progress >= 100">100%</td>
+    <td v-else>Waiting...</td>
 
-        <td v-if="job.progress >= 100">
-          <JobResult :job="job"/>
-        </td>
-        <td v-else>Loading...</td>
-      </tr>
+    <td v-if="job.progress >= 100">
+      <JobResult :job="job" />
+    </td>
+    <td v-else>Loading...</td>
+  </tr>
 </template>
 
 <script>
-import JobResult from './BatchJobResult';
+import JobResult from "./BatchJobResult";
 
 export default {
   data() {
-    return {
-    };
+    return {};
   },
   methods: {
     changeTimeHuman(time) {
@@ -42,27 +43,31 @@ export default {
       let HumanTime = HumanDate.split("T", 2).join(" ");
       return HumanTime;
     },
-    getJobName(jobType){
-      switch(jobType){
-        case "extract_keywords": return "Extract Keywords";
-        case "refresh_orb": return "Refresh ORB";
-        case "export_companies": return "Export Companies";
-        case "refresh_news": return "Refresh News";
+    getJobName(jobType) {
+      switch (jobType) {
+        case "extract_keywords":
+          return "Extract Keywords";
+        case "refresh_orb":
+          return "Refresh ORB";
+        case "export_companies":
+          return "Export Companies";
+        case "refresh_news":
+          return "Refresh News";
       }
     }
   },
   props: {
     job: Object
   },
-  computed:{
-    additionalDataParsed: function(){
-        if (!!this.job && !! this.job.additionalData){
-            return JSON.parse(this.job.additionalData);
-        }
-        return {}
+  computed: {
+    additionalDataParsed: function() {
+      if (!!this.job && !!this.job.additionalData) {
+        return JSON.parse(this.job.additionalData);
+      }
+      return "";
     }
   },
-  components:{
+  components: {
     JobResult
   }
 };
