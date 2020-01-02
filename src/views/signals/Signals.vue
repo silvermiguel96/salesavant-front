ab23918f389e15464baa8676a5c999a6b91477d1
Alejandro <lariverosc@gmail.com>
2020-01-02 01:50:41 -0500

    Fix and make homogeneus signals view

--- src/views/signals/Signals.vue
+++ src/views/signals/Signals.vue
@@ -1,127 +1,72 @@
 <template>
   <v-container fluid>
     <v-card>
-      <div class="apollo-example">
-        <v-breadcrumbs
+      <v-breadcrumbs
         :large="true"
-          v-if="!!this.$route.query && !!this.$route.query.searchType"
-          :items="[
-            {
-              text: 'Signals',
-              disabled: false,
-              href: '/signals'
-            },
-            {
-              text: `${this.$route.query.searchType} search`,
-              disabled: true,
-              href: '/companies'
-            }
-          ]"
-          divider=">"
-        ></v-breadcrumbs>
-        <v-breadcrumbs
-        :large="true"
-          v-else
-          :items="[
-            {
-              text: 'Signals',
-              disabled: true,
-              href: '/signals'
-            }
-          ]"
-          divider=">"
-        ></v-breadcrumbs>
-        <v-btn
-          small
-          class="ma-2 text-capitalize"
-          color="primary"
-          dark
-          @click="toggleSearch"
-        >
-          <v-icon small class="pr-1">search</v-icon>search
-        </v-btn>
-        <v-btn small class="ma-2 text-capitalize" to="/signals/create">
-          <v-icon small class="pr-1">add</v-icon>new signal
-        </v-btn>
-
-        <template
-          v-if="
-            !!this.advancedSearch.searchType &&
-              this.advancedSearch.searchType == 'signals'
-          "
-        >
-          <ApolloQuery
-            :query="require('./graphql/SearchsSignals.gql')"
-            :variables="{
-              ...this.advancedSearch.signalSearch,
-              first: this.itemsPerPage,
-              offset: this.itemsPerPage * this.page - this.itemsPerPage  
-            }"
-            :deep="true"
-          >
-            <template slot-scope="{ result: { loading, error, data } }">
-              <!-- Loading -->
-              <div v-if="loading" class="loading apollo">Loading...</div>
-
-              <!-- Error -->
-              <!--<div v-else-if="error" class="error apollo">An error occured</div>-->
-
-              <!-- Result -->
-              <div v-else-if="data" class="result apollo">
-                <!---<div>{{ JSON.stringify(data) }}</div>-->
-                <signals-table
-                  v-if="data.signals.signalsList"
-                  @deleteSignal="deleteSignal"
-                  :signalId="signalId"
-                  :items="data.signals.signalsList"
-                  :totalResults="data.signals.totalResults"
-                  class="result apollo"
-                  @updateOptions="updateOptions"
-                ></signals-table>
+        :items="[
+          {
+            text: 'Signals',
+            disabled: true,
+            href: '/signals'
+          }
+        ]"
+        divider=">"
+      ></v-breadcrumbs>
+      <v-container fluid class="mx-1">
+        <v-row no-gutters class="ml-2">
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Filter"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="8">
+            <div class="d-flex justify-md-end">
+              <div class="mt-3 mr-2">
+                <v-btn class="text-capitalize"  color="primary"  min-width="150" to="/signals/create">
+                  <v-icon size="18" class="mr-2">add</v-icon>new signal
+                </v-btn>
               </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+      <ApolloQuery
+        :query="require('./graphql/Signals.gql')"
+        :variables="{
+          search: this.search,
+          first: this.itemsPerPage,
+          offset: this.itemsPerPage * this.page - this.itemsPerPage
+        }"
+        :skip="search.length > 0 && search.length < 2"
+      >
+        <template slot-scope="{ result: { loading, error, data } }">
+          <!-- Loading -->
+          <div v-if="loading" class="loading apollo">Loading...</div>
 
-              <!-- No result -->
-              <div v-else class="no-result apollo">Loading...</div>
-            </template>
-          </ApolloQuery>
+          <!-- Error -->
+          <!--<div v-else-if="error" class="error apollo">An error occured</div>-->
+
+          <!-- Result -->
+          <div v-else-if="data" class="result apollo">
+            <!---<div>{{ JSON.stringify(data) }}</div>-->
+            <signals-table
+              v-if="data.signals.totalResults"
+              @deleteSignal="deleteSignal"
+              :items="data.signals.signalsList"
+              :totalResults="data.signals.totalResults"
+              class="result apollo"
+              @updateOptions="updateOptions"
+            ></signals-table>
+          </div>
+
+          <!-- No result -->
+          <div v-else class="no-result apollo">Loading...</div>
         </template>
-        <!-- Apollo watched Graphql query -->
-        <template v-else>
-          <ApolloQuery
-            :query="require('./graphql/Signals.gql')"
-            :variables="{
-              first: this.itemsPerPage,
-              offset: this.itemsPerPage * this.page - this.itemsPerPage   
-            }"
-            :deep="true"
-          >
-            <template slot-scope="{ result: { loading, error, data } }">
-              <!-- Loading -->
-              <div v-if="loading" class="loading apollo">Loading...</div>
-
-              <!-- Error -->
-              <!--<div v-else-if="error" class="error apollo">An error occured</div>-->
-
-              <!-- Result -->
-              <div v-else-if="data" class="result apollo">
-                <!---<div>{{ JSON.stringify(data) }}</div>-->
-                <signals-table
-                  v-if="data.signals.totalResults"
-                  @deleteSignal="deleteSignal"
-                  :signalId="signalId"
-                  :items="data.signals.signalsList"
-                  :totalResults="data.signals.totalResults"
-                  class="result apollo"
-                  @updateOptions="updateOptions"
-                ></signals-table>
-              </div>
-
-              <!-- No result -->
-              <div v-else class="no-result apollo">Loading...</div>
-            </template>
-          </ApolloQuery>
-        </template>
-      </div>
+      </ApolloQuery>
     </v-card>
   </v-container>
 </template>
@@ -135,17 +80,18 @@ import { mapMutations } from "../../store";
 export default {
   data() {
     return {
-      showTable: true,
       items: ["signals"],
       page: 1,
       itemsPerPage: 10,
-      descending: false,
       sortBy: "",
       signalId: "",
-      isFiltered: false
+      sortOrder: "",
+      search: ""
     };
   },
-  components: { SignalsTable },
+  components: {
+    SignalsTable
+  },
   methods: {
     updateOptions({
       dataFromEvent: { page = 1, itemsPerPage = 10, sortBy = [], sortDesc = [] }
@@ -175,24 +121,13 @@ export default {
         console.log("Result", result);
         this.$router.go(this.$router.currentRoute);
         console.log(this.$apollo.queries);
-        //this.$apollo.queries.AllSignals.skip = false;
-        //this.$apollo.queries.AllSignals.refetch();
         return;
       } catch (error) {
-        this.snack = true;
-        this.snackColor = "error";
-        this.snackText = "oops we did something wrong!";
         console.log("error delete signal", error);
       } finally {
         this.showTable = true;
       }
-    },
-    toggleSearch() {
-      this.$store.commit('showSearchDialog')
-    },
-  },
-  props: {
-    showSearch: { type: Boolean, default: false }
+    }
   }
 };
 </script>
