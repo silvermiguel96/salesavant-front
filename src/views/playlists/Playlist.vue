a405cf1a85a1e97e4e305e9ab2b888e24db4999f
Alejandro <lariverosc@gmail.com>
2020-01-04 02:14:42 -0500

    Refacor playlist view as a tabbed view

--- src/views/playlists/Playlist.vue
+++ src/views/playlists/Playlist.vue
@@ -1,9 +1,8 @@
 <template>
   <v-container fluid>
-    <v-card>
-      <v-breadcrumbs
-        :large="true"
-        :items="[
+    <v-breadcrumbs
+      :large="true"
+      :items="[
             {
               text: 'Playlists',
               disabled: false,
@@ -14,44 +13,43 @@
               disabled: true
             }
           ]"
-        divider=">"
-      >
-        <template v-slot:item="props">
-          <v-breadcrumbs-item
-            :href="props.item.href"
-            :class="[props.item.disabled && 'disabled']"
-            @click.prevent="$router.push(props.item.href)">
-            {{ props.item.text }}
-          </v-breadcrumbs-item>
-        </template>
-      </v-breadcrumbs>
-      <v-tabs grow background-color="grey lighten-5" color="primary">
-        <!-- Tab Companies -->
-        <v-tab>Companies</v-tab>
-        <v-tab-item>
-          <companies />
-        </v-tab-item>
-        <!-- tab signals -->
-        <v-tab>Signals</v-tab>
-        <v-tab-item>
-          <div>
-            <h4>Coming soon</h4>
-          </div>
-        </v-tab-item>
-        <!-- Tab contacts -->
-        <v-tab>Contacts</v-tab>
-        <v-tab-item>
-          <div>
-            <h4>Coming soon</h4>
-          </div>
-        </v-tab-item>
-        <!-- Tab analytics -->
-        <v-tab>Analytics</v-tab>
-        <v-tab-item>
-          <contact></contact>
-        </v-tab-item>
-      </v-tabs>
-    </v-card>
+      divider=">"
+    >
+      <template v-slot:item="props">
+        <v-breadcrumbs-item
+          :href="props.item.href"
+          :class="[props.item.disabled && 'disabled']"
+          @click.prevent="$router.push(props.item.href)"
+        >{{ props.item.text }}</v-breadcrumbs-item>
+      </template>
+    </v-breadcrumbs>
+    <v-tabs grow background-color="grey lighten-5" color="primary">
+      <!-- Tab Companies -->
+      <v-tab>Companies</v-tab>
+      <v-tab-item>
+        <companies />
+      </v-tab-item>
+      <!-- tab signals -->
+      <v-tab>Signals</v-tab>
+      <v-tab-item>
+        <div>
+          <h4>Coming soon</h4>
+        </div>
+      </v-tab-item>
+      <!-- Tab contacts -->
+      <v-tab>Contacts</v-tab>
+      <v-tab-item>
+        <div>
+          <h4>Coming soon</h4>
+        </div>
+      </v-tab-item>
+      <!-- Tab analytics -->
+      <v-tab>Analytics</v-tab>
+      <v-tab-item>
+        <analytics :aggs_data="additionalDataParsed">
+        </analytics>
+      </v-tab-item>
+    </v-tabs>
   </v-container>
 </template>
 
@@ -59,21 +57,16 @@
 import _get from "lodash.get";
 import gql from "graphql-tag";
 import Companies from "./components/Companies.vue";
+import Analytics from "./components/Analytics.vue";
 import { mapMutations } from "vuex";
-
 
 export default {
   components: {
-    Companies
+    Companies,
+    Analytics
   },
   data() {
     return {
-      name: "",
-      descending: false,
-      page: 1,
-      itemsPerPage: 10,
-      sortBy: "",
-      search: "",
       isLoading: false,
       playlist: {
         uid: "",
@@ -92,6 +85,7 @@ export default {
             name
             totalScore
             description
+            additionalData
           }
         }
       `,
@@ -104,32 +98,20 @@ export default {
     }
   },
   methods: {
-    ...mapMutations(["updateCompanySearch"]),
-    updateOptions({
-      dataFromEvent: { page = 1, itemsPerPage = 10, sortBy = [], sortDesc = [] }
-    }) {
-      this.page = page;
-      this.itemsPerPage = itemsPerPage;
-    },
-    createJob(jobType) {
-      this.$emit("createJob", {
-        jobType: jobType,
-        additionalData: {
-          playlist_uid: this.playlist.uid,
-          playlist_name: this.playlist.name
-        }
-      });
-      this.$emit("showSnack", `Job ${jobType} enqueed successfully`, "success");
+    ...mapMutations(["updateCompanySearch"])
+  },
+  computed: {
+    additionalDataParsed: function() {
+      if (!!this.playlist && !!this.playlist.additionalData) {
+        return JSON.parse(this.playlist.additionalData);
+      }
+      return {};
     }
   },
   beforeCreate() {
     this.$apollo.query.playlist;
   },
   mounted() {
-    console.log(
-      "playlist-companies update state ",
-      this.$route.params.playlistUid
-    );
     this.$store.commit("updateCompanySearch", {
       playlistUid: this.$route.params.playlistUid
     });
