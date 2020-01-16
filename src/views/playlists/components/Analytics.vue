e70f31b5282dbcb801c94d9e69cda581a4cc27b4
Alejandro <lariverosc@gmail.com>
2020-01-15 23:53:02 -0500

    Improve analytics chart stylyng and conf management

--- src/views/playlists/components/Analytics.vue
+++ src/views/playlists/components/Analytics.vue
@@ -5,7 +5,7 @@
         <div class="headline">Top Signals</div>
       </v-card-subtitle>
       <v-card-text>
-        <c3-chart :config="config" :type="'bar'" :data="aggs_top_signals" />
+        <c3-chart :type="'bar'" :config="aggs_top_signals_config" :data="aggs_top_signals" />
       </v-card-text>
     </v-card>
 
@@ -14,7 +14,7 @@
         <div class="headline">Top NAICS Descriptions</div>
       </v-card-subtitle>
       <v-card-text>
-        <c3-chart :config="config" :type="'bar'" :data="aggs_top_naics_code" />
+        <c3-chart :type="'bar'" :config="config" :data="aggs_top_naics_code" />
       </v-card-text>
     </v-card>
   </div>
@@ -23,29 +23,53 @@
 <script>
 import C3Chart from "../../../components/C3Chart";
 export default {
+  data() {
+    return {
+      topSignalsCategories: []
+    };
+  },
   props: {
-    config: {
-      bar: {
-        width: {
-          ratio: 0.5
-        }
-      }
-    },
     aggs_data: { type: Object, default: () => {} }
   },
   methods: {
-    parse_agg_string: function(agg_string) {
-      return agg_string.split("||").map(entry => {
+    parse_aggs_top_signals: function(agg_string) {
+      let temp = [];
+      let counts = agg_string.split("||").map(entry => {
         let entrySplit = entry.split(">>");
-        return [entrySplit[0], parseInt(entrySplit[1])];
+        temp.push(entrySplit[1]);
+        return parseInt(entrySplit[2]);
       });
+      this.topSignalsCategories = temp;
+      return counts;
     }
   },
   computed: {
     aggs_top_signals: function() {
       if (this.aggs_data.hasOwnProperty("aggs_top_signals")) {
         return {
-          columns: this.parse_agg_string(this.aggs_data.aggs_top_signals)
+          columns: [
+            this.parse_aggs_top_signals(this.aggs_data.aggs_top_signals)
+          ]
+        };
+      }
+      return null;
+    },
+
+    aggs_top_signals_config: function() {
+      if (this.aggs_data.hasOwnProperty("aggs_top_signals")) {
+        return {
+          bar: {
+            space: 0.05
+          },
+          axis: {
+            x: {
+              type: "category",
+              categories: this.topSignalsCategories,
+            }
+          },
+          legend: {
+            hide: true
+          }
         };
       }
       return null;
