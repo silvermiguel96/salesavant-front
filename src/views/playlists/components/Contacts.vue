afb4e588d0a4732f060c0b1819df6c71527de31d
Alejandro <lariverosc@gmail.com>
2020-03-15 20:29:44 -0500

    Refactor playlist contacts to use contactsTable component

--- src/views/playlists/components/Contacts.vue
+++ src/views/playlists/components/Contacts.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container class="pa-0" fluid>
     <v-card>
       <div class="apollo-example">
         <v-container fluid class="mx-1">
@@ -15,96 +15,29 @@
             </v-col>
           </v-row>
         </v-container>
-        <!-- Apollo watched Graphql query -->
-        <v-data-table
-          v-if="!!playlistContacts"
-          :headers="headers"
-          :items="playlistContacts.contactsList"
-          :server-items-length="playlistContacts.totalResults"
-          :items-per-page="options.itemsPerPage"
-          :footer-props="{
-            'items-per-page-options': [10, 20, 50]
-          }"
-          :options.sync="options"
-          class="mx-2"
-          @update:options="updateOptions"
-          :single-expand="true"
-          expanded.sync="expanded"
-          item-key="uid"
-          show-expand
-        >
-          <template v-slot:item.fullName="{ item }">
-            <router-link
-              class="subtitle-2 font-weight-medium"
-              :to="`/contacts/${item.uid}`"
-            >{{ item.fullName }}</router-link>
-          </template>
-          <template v-slot:item.companies.title="{ item }">
-            <div v-for="select in item.companies" :key="select.uid">
-              <span v-if="select.isCurrent">{{ select.title }}</span>
-            </div>
-          </template>
-          <template v-slot:item.companies.name="{ item }">
-            <div v-for="select in item.companies" :key="select.uid">
-              <router-link
-                v-if="select.isCurrent"
-                :to="`/companies/${select.company.uid}`"
-              >{{ select.company.name }}</router-link>
-            </div>
-          </template>
-          <template v-slot:item.companies.deparment="{ item }">
-            <p v-if="item.companies.departament">{{ item.companies.departament || "--" }}</p>
-            <p v-else>--</p>
-          </template>
-          <template v-slot:item.companies.rank="{ item }">
-            <p v-if="item.companies.rank">{{ item.companies.rank }}</p>
-            <p v-else>--</p>
-          </template>
-          <template v-slot:item.scaleScoreAverage="{ item }">
-            <p v-if="item.scaleScoreAverage">{{ item.scaleScoreAverage }}</p>
-            <p v-else>--</p>
-          </template>
-          <template v-slot:item.capitalEfficiencyScoreAverage="{ item }">
-            <p v-if="item.capitalEfficiencyScoreAverage">{{ item.capitalEfficiencyScoreAverage }}</p>
-            <p v-else>--</p>
-          </template>
-          <template v-slot:item.capitalEfficiencyEstimateAverage="{ item }">
-            <p v-if="item.capitalEfficiencyEstimateAverage">{{ item.capitalEfficiencyEstimateAverage }}</p>
-            <p v-else>--</p>
-          </template>
-          <template v-slot:item.numberOfExits="{ item }">
-            <p v-if="item.numberOfExits">{{ item.numberOfExits }}</p>
-            <p v-else>--</p>
-          </template>
-          <template v-slot:expanded-item="{ headers, item }">
-            <td class="ma-0 pa-0" :colspan="headers.length">
-              <v-simple-table>
-                <template v-slot:default>
-                  <tbody>
-                    <tr v-for="job in item.companies" :key="job.uid">
-                      <td style="width:4%;"></td>
-                      <td style="width:15%;"></td>
-                      <td style="width:15%;">{{ job.title || "" }}</td>
-                      <td style="width:15%;">
-                        <router-link
-                          :to="`/companies/${job.company.uid}`"
-                        >{{ job.company.name || "" }}</router-link>
-                      </td>
-                      <td style="width:10%;"></td>
-                      <td style="width:10%;"></td>
-                      <td style="width:6%;">{{ job.company.scaleScore || "--" }}</td>
-                      <td style="width:6%;">{{ job.company.capitalEfficiencyScore || "--" }}</td>
-                      <td style="width:6%;">{{ job.company.capitalEfficiencyEstimate || "--" }}</td>
-                      <td style="width:6%;">--</td>
-                    </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
-              <v-divider></v-divider>
-              <!-- </div> -->
-            </td>
-          </template>
-        </v-data-table>
+        <div>
+          <!-- Result -->
+          <contacts-table
+            v-if="playlistContacts"
+            :items="playlistContacts.contactsList"
+            :totalResults="playlistContacts.totalResults"
+            @updateOptions="updateOptions"
+          ></contacts-table>
+          <!-- Loading -->
+          <v-row justify="center" no-gutters>
+              <v-col cols="12">
+                <v-progress-linear
+                :active="!!isLoading"
+                color="blue"
+                indeterminate
+                absolute
+                bottom
+                query
+                ></v-progress-linear>
+              </v-col>
+          </v-row>
+          </div>
+        
       </div>
     </v-card>
   </v-container>
@@ -112,6 +45,7 @@
 
 <script>
 import gql from "graphql-tag";
+import ContactsTable from "../../../components/contacts/ContactsTable";
 
 export default {
   data() {
@@ -177,15 +111,18 @@ export default {
           align: "left"
         }
       ],
-      items: [],
       search: "",
-      sortBy:"",
-      sortOrder:"",
+      isLoading: true,
       options: {
         page: 1,
-        itemsPerPage: 10
+        itemsPerPage: 10,
+        sortBy:"",
+        sortOrder:""
       }
     };
+  },
+  components:{
+    ContactsTable
   },
   methods: {
     updateOptions({
@@ -197,25 +134,25 @@ export default {
       if (sortBy.length > 0) {
         switch (sortBy[0]) {
           case "scaleScoreAverage":
-            this.sortBy = "scale_score_average";
+            this.options.sortBy = "scale_score_average";
           case "capitalEfficiencyScoreAverage":
-            this.sortBy  = "capital_efficiency_score_average";
+            this.options.sortBy  = "capital_efficiency_score_average";
           case "capitalEfficiencyEstimateAverage":
-            this.sortBy  = "capitalEfficiencyEstimate_average";
+            this.options.sortBy  = "capitalEfficiencyEstimate_average";
           case "numberOfExits":
-            this.sortBy  = "number_of_exits";
+            this.options.sortBy  = "number_of_exits";
         }
       } else {
-        this.sortBy  = "";
+        this.options.sortBy  = "";
       }
       if (sortDesc.length > 0) {
         if (sortDesc[0]) {
-          this.sortOrder = "desc";
+          this.options.sortOrder = "desc";
         } else {
-          this.sortOrder = "asc";
+          this.options.sortOrder = "asc";
         }
       } else {
-        this.sortOrder = "";
+        this.options.sortOrder = "";
       }
     }
   },
@@ -253,14 +190,17 @@ export default {
         return {
           playlistUid: this.$route.params.playlistUid,
           search: this.search,
-          sortBy: this.sortBy,
-          sortOrder: this.sortOrder,
+          sortBy: this.options.sortBy,
+          sortOrder: this.options.sortOrder,
           first: this.options.itemsPerPage,
           offset: this.options.itemsPerPage * this.options.page - this.options.itemsPerPage,
         };
       },
       skip(){
         return this.search.length > 0 && this.search.length < 2;
+      },
+      watchLoading (isLoading, countModifier) {
+        this.isLoading = isLoading;
       },
       fetchPolicy: "cache-and-network"
     }
