26c49ab7ff6369b510be8f23c7c2a3b5b2a13536
Miguelangel <silvermiguel96@gmail.com>
2020-03-23 11:35:02 -0500

    Add new fields scaleScore and capitalEfficiencyScore

--- src/views/contacts/Contacts.vue
+++ src/views/contacts/Contacts.vue
@@ -166,6 +166,8 @@ export default {
                 company {
                   uid
                   name
+                  scaleScore
+                  capitalEfficiencyScore
                 }
               }
             }
