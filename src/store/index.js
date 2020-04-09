60bbbe68221bcf3d809d5b5b3c50365e51418b9f
Alejandro <lariverosc@gmail.com>
2020-04-09 00:23:51 -0500

    Modify store model to support two kind of searches

--- src/store/index.js
+++ src/store/index.js
@@ -17,50 +17,95 @@ export const defaultCompanySearch = {
   moreThanScore: 0,
   lessThanScore: 0,
   playlistUid: "",
-  displayPlaylistUid: "",
-  signals: [],
-  displaySignals: [],
-  signalGroups: []
+  displayPlaylistUid: ""
+};
+
+export const defaultContactSearch = {
+  name: "",
+  title: "",
+  department: "",
+  rank: "",
+  city: "",
+  state: "",
+  region: "",
+  country: "",
+  status: "",
+  moreThanScaleScoreAverage: 0,
+  lessThanScaleScoreAverage: 0,
+  moreThanCesa: 0,
+  lessThanCesa: 0,
+  moreThanWolfpackScore: 0,
+  lessThanWolfpackScore: 0,
+  moreThanNumberOfExits: 0,
+  lessThanNumberOfExits: 0,
+  playlistUid: "",
+  displayPlaylistUid: ""
 };
 
 const state = {
-  advancedSearch: {
-    showDialog: false,
-    searchType: null,
-    companySearch: { ...defaultCompanySearch },
+  userEmail: null,
+  jwtToken: null,
+  searchType: null,
+  showSearchDialog: false,
+  companySearch: {
+    ...defaultCompanySearch
+  },
+  contactSearch: {
+    ...defaultContactSearch
+  },
+};
+
+const getters = {
+  isLoggedIn: state => {
+    return state.jwtToken ? true : false;
   }
 };
 
-const getters = {};
-
 const mutations = {
-  showSearchDialog() {
-    state.advancedSearch.showDialog = true;
+  showSearchDialog(state) {
+    state.showSearchDialog = true;
   },
-  hideSearchDialog() {
-    state.advancedSearch.showDialog = false;
+  hideSearchDialog(state) {
+    state.showSearchDialog = false;
   },
-  resetAdvancedSearch() {
-    state.advancedSearch = {
-      showDialog: false,
-      searchType: null,
+  resetCompanySearch(state) {
+    state = {
+      ...state,
       companySearch: { ...defaultCompanySearch }
     };
   },
-  showDenseForm() {
-    state.denseForm = false
+  resetContactSearch(state) {
+    state = {
+      ...state,
+      contactSearch: { ...defaultContactSearch }
+    };
   },
   updateCompanySearch(state, newCompanySearch) {
-    state.advancedSearch.companySearch = {
-      ...state.advancedSearch.companySearch,
+    state.companySearch = {
+      ...state.companySearch,
       ...newCompanySearch
     };
   },
   doCompanySearch(state, companySearch) {
     console.log(companySearch);
-    state.advancedSearch.searchType = "companies";
-    state.advancedSearch.companySearch = companySearch;
-    state.advancedSearch.showDialog = false;
+    state.searchType = "companies";
+    state.companySearch = companySearch;
+    state.companySearch.showDialog = false;
+  },
+  updateContactSearch(state, newContactSearch) {
+    state.contactSearch = {
+      ...state.contactSearch,
+      ...newContactSearch
+    };
+  },
+  doContactSearch(state, contactSearch) {
+    console.log(contactSearch);
+    state.searchType = "contacts";
+    state.contactSearch = contactSearch;
+    state.contactSearch.showDialog = false;
+  },
+  showDenseForm(state) {
+    state.denseForm = false
   }
 };
 
