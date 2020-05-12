086fcc6e27d8f70170517a6d46c18291559df354
Miguelangel <silvermiguel96@gmail.com>
2020-05-11 20:43:15 -0500

    Nab bar improvements

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -8,7 +8,23 @@
               <v-icon class="white--text">{{ link.icon }}</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title class="white--text " :to="link.router">{{ link.text }}</v-list-item-title>
+              <v-list-item-title class="white--text" :to="link.router">{{ link.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/account" @click="onAccount">
+            <v-list-item-icon>
+              <v-icon class="white--text">account_circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="white--text" to="/account">My perfil</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/home" @click="exitApp">
+            <v-list-item-icon>
+              <v-icon class="white--text">exit_to_app</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="white--text" to="/home">Login out</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
@@ -20,15 +36,22 @@
         <img class="logo mt-1" :src="require('../assets/salesavant_header.png')" />
       </router-link>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down " v-for="link in links" :key="link.text">
+      <v-toolbar-items class="hidden-sm-and-down" v-for="link in links" :key="link.text">
         <v-btn v-if="!!link.click" text class="text-capitalize" @click="link.click">{{link.text}}</v-btn>
         <v-btn v-else text class="text-capitalize" :to="link.router">{{link.text}}</v-btn>
       </v-toolbar-items>
-      <v-toolbar-items>
-        <v-btn text color="white" class="text-capitalize" @click="this.exitApp">
-            Logout
-        </v-btn>
-      </v-toolbar-items>
+      <v-menu left bottom offset-y>
+        <template class="hidden-sm-and-down" v-slot:activator="{ on }">
+          <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn text class="text-capitalize" v-on="on">Account</v-btn>
+          </v-toolbar-items>
+        </template>
+        <v-list color="primary" class="pa-0">
+          <v-list-item v-for="account in accounts" :key="account.text" @click="account.click">
+            <v-list-item-title class="white--text subtitle-1 font-weight-medium">{{ account.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-toolbar>
   </nav>
 </template>
@@ -65,25 +88,29 @@ export default {
           router: "/signals",
           click: this.onSignals
         },
-        { 
-          icon: "work", 
+        {
+          icon: "work",
           text: "Batch",
           click: this.onBatch
-        },
-        { 
-          icon: "account_circle", 
-          text: "Account",
-          click: this.onAccount
         }
+      ],
+      accounts: [
+        {
+          text: "My perfil",
+          router: "/account",
+          click: this.onAccount
+        },
+        {
+          text: "Login Out",
+          router: "/home",
+          click: this.exitApp
+        },
       ]
     };
   },
-  props: {
-  },
+  props: {},
   methods: {
-    ...mapMutations([
-      'resetCompanySearch', 'resetContactSearch'
-    ]),
+    ...mapMutations(["resetCompanySearch", "resetContactSearch"]),
     onBatch() {
       this.resetSearch();
       this.$router.push("/batch").catch(err => {});
@@ -112,7 +139,7 @@ export default {
       localStorage.removeItem(AUTH_TOKEN);
       this.$router.go("/login");
     },
-    resetSearch(){
+    resetSearch() {
       this.resetCompanySearch();
       this.resetContactSearch();
     }
