f15957b61c7cc80a1fc1cb3f63fe689f88f75f77
Alejandro <lariverosc@gmail.com>
2020-05-25 18:23:51 -0500

    Small adjustments

--- src/components/companies/company/Content.vue
+++ src/components/companies/company/Content.vue
@@ -3,43 +3,35 @@
     <v-row>
       <v-col md="6" xs="12" class="px-0 py-0 pb-1 pr-sm-3 pb-sm-0">
         <v-card v-if="company" style="height:100%;" class="pa-3">
-          <v-row no-gutters>
-            <v-col cols="12">
-              <v-card-text>
-              <div class="title text-capitalize font-weight-regular grey--text">Website Text</div>
-                <v-row no-gutters>
-                  <v-col cols="12" class="pr-2" >
-                    <long-paragraph
-                      class="font-weight-light text-justify pt-1"
-                      :text="company.website"
-                      :maxLength="1500"
-                    ></long-paragraph>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-col>
-          </v-row>
+            <v-card-text>
+              <div class="subtitle-1 text-capitalize font-weight-medium">Website Text</div>
+              <long-paragraph
+                class="font-weight-normal text-justify mt-4"
+                :text="company.website"
+                :maxLength="1500"
+              ></long-paragraph>
+            </v-card-text>
         </v-card>
       </v-col>
       <v-col md="6" xs="12" class="px-0 py-0">
         <v-card v-if="company" style="height:100%;" class="pa-3">
           <v-card-text>
-            <!-- The first table -->
-            <div>
-              <div class="title text-capitalize font-weight-regular grey--text">Web Site Links</div>
-              <v-spacer></v-spacer>
+              <div class="subtitle-1 text-capitalize font-weight-medium">Web Site Links</div>
               <v-text-field
                 v-model="search"
                 append-icon="filter_list"
                 label="Quick Search"
                 placeholder="Type a Keyword"
+                class="pt-3"
                 hide-details
               ></v-text-field>
-              <v-data-table :headers="headers" :items="httpCompany" :search="search" dense>
+              <v-row justify="center" no-gutters>
+                <v-col cols="12">
+              <v-data-table :headers="headers" :items="httpCompany" :search="search" class="mt-4" dense>
                 <template v-slot:item="{ item }">
                   <tr>
                     <td>
-                        {{ item.linkLabel}}
+                        {{ item.linkLabel || '--' }}
                     </td>
                     <td>
                       <a
@@ -51,14 +43,15 @@
                         <long-paragraph
                           class="font-weight-light text-justify"
                           :text="item.url"
-                          :maxLength="60"
+                          :maxLength="35"
                         ></long-paragraph>
                       </a>
                     </td>
                   </tr>
                 </template>
               </v-data-table>
-            </div>
+              </v-col>
+              </v-row>
           </v-card-text>
         </v-card>
       </v-col>
@@ -75,8 +68,8 @@ export default {
       company: null,
       search: "",
       headers: [
-        { text: "Names", value: "linkLabel", sortable: true , width: "20%" },
-        { text: "Links", value: "url", sortable: false , width: "80%"}
+        { text: "Label", value: "linkLabel", sortable: false , width: "40%" },
+        { text: "Link", value: "url", sortable: false , width: "60%"}
       ]
     };
   },
