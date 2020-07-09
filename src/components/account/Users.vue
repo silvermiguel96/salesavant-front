2df160df25076d55839d66624319ad76d4279c77
Miguelangel <silvermiguel96@gmail.com>
2020-07-09 15:10:28 -0500

    Create new router an table user.

---
+++ src/components/account/Users.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container fluid>
+    <v-container fluid>
+      <v-row no-gutters>
+        <v-col cols="12" sm="3" md="3" lg="2" class="pa-1">
+          <v-btn class="text-capitalize" block color="primary" min-width="150" to="/launch-job">
+            <v-icon size="18" class="mr-2">add</v-icon>new job
+          </v-btn>
+        </v-col>
+        <v-row no-gutters class="d-flex justify-end">
+          <v-col cols="12" sm="6" md="6" lg="6" class="pa-1">
+            <v-text-field
+              v-model="search"
+              append-icon="filter_list"
+              label="Quick Search"
+              hide-details
+              placeholder="Type a Job Name or Description"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-row>
+    </v-container>
+    <!-- Result -->
+    <div v-if="accounts" class="result">
+      <user-table
+        v-if="this.accounts"
+        :items="this.accounts[0].users"
+        :totalResults="this.accounts[0].users.length"
+        @updateOptions="updateOptions"
+      ></user-table>
+    </div>
+    <div v-else class="no-result">Loading...</div>
+  </v-container>
+</template>
+<script>
+import gql from "graphql-tag";
+import { setTimeout } from "timers";
+import UserTable from "./UsersTable.vue";
+
+export default {
+  data() {
+    return {
+      items: [],
+      totalResults: 0,
+      page: 1,
+      itemsPerPage: 10,
+      search: ""
+    };
+  },
+  methods: {
+    updateOptions({ dataFromEvent: { page = 1, itemsPerPage = 10 } }) {
+      this.page = page;
+      this.itemsPerPage = itemsPerPage;
+      this.loadData();
+    }
+  },
+  components: {
+    UserTable
+  },
+  apollo: {
+    accounts: {
+      query: gql`
+        query {
+          accounts {
+            users {
+              id
+              lastName
+              firstName
+              email
+              status
+            }
+          }
+        }
+      `
+    }
+  }
+};
+</script>
