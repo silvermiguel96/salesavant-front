8f6d9b691037f9b8264aeac95010bd63a43e26f9
Miguelangel <silvermiguel96@gmail.com>
2020-07-10 14:47:49 -0500

    resolver comment in github

--- src/components/batch/Jobs/ScheduledJobs.vue
+++ src/components/batch/Jobs/ScheduledJobs.vue
@@ -1,36 +1,35 @@
 <template>
   <v-container fluid>
-      <v-container fluid>
-        <v-row no-gutters>
-          <v-col cols="12" sm="3" md="3" lg="2" class="pa-1">
-            <v-btn class="text-capitalize" block color="primary" min-width="150" to="/launch-job">
-              <v-icon size="18" class="mr-2">add</v-icon>new job
-            </v-btn>
+    <v-container fluid>
+      <v-row no-gutters>
+        <v-col cols="12" sm="3" md="3" lg="2" class="pa-1">
+          <v-btn class="text-capitalize" block color="primary" min-width="150" to="/launch-job">
+            <v-icon size="18" class="mr-2">add</v-icon>new job
+          </v-btn>
+        </v-col>
+        <v-row no-gutters class="d-flex justify-end">
+          <v-col cols="12" sm="6" md="6" lg="6" class="pa-1">
+            <v-text-field
+              v-model="search"
+              append-icon="filter_list"
+              label="Quick Search"
+              hide-details
+              placeholder="Type a Job Name or Description"
+            ></v-text-field>
           </v-col>
-          <v-row no-gutters class="d-flex justify-end">
-            <v-col cols="12" sm="6" md="6" lg="6" class="pa-1">
-              <v-text-field
-                v-model="search"
-                append-icon="filter_list"
-                label="Quick Search"
-                hide-details
-                placeholder="Type a Job Name or Description"
-                @change="onSearch"
-              ></v-text-field>
-            </v-col>
-          </v-row>
         </v-row>
-      </v-container>
-      <!-- Result -->
-      <div v-if="items" class="result">
-        <scheduled-jobs-table
-          v-if="items"
-          :items="this.items"
-          :totalResults="this.totalResults"
-          @updateOptions="updateOptions"
-        ></scheduled-jobs-table>
-      </div>
-      <div v-else class="no-result">Loading...</div>
+      </v-row>
+    </v-container>
+    <!-- Result -->
+    <div v-if="salesavantScheduledJobs" class="result">
+      <scheduled-jobs-table
+        v-if="salesavantScheduledJobs"
+        :items="this.salesavantScheduledJobs.salesavantJobsList"
+        :totalResults="this.salesavantScheduledJobs.totalResults"
+        @updateOptions="updateOptions"
+      ></scheduled-jobs-table>
+    </div>
+    <div v-else class="no-result">Loading...</div>
   </v-container>
 </template>
 <script>
@@ -41,23 +40,25 @@ import ScheduledJobsTable from "../../batch/ScheduledJobsTable.vue";
 export default {
   data() {
     return {
-      items: [],
       totalResults: 0,
       page: 1,
       itemsPerPage: 10,
       search: ""
     };
   },
-  methods: {
-    updateOptions({ dataFromEvent: { page = 1, itemsPerPage = 10 } }) {
-      this.page = page;
-      this.itemsPerPage = itemsPerPage;
-      this.loadData();
-    },
-    loadData: function() {
-      const salesavantScheduledJobs = gql`
-        query salesavantScheduledJobs($search: String, $first: Int, $offset: Int) {
-          salesavantScheduledJobs(search: $search, first: $first, offset: $offset) {
+  apollo: {
+    salesavantScheduledJobs: {
+      query: gql`
+        query salesavantScheduledJobs(
+          $search: String
+          $first: Int
+          $offset: Int
+        ) {
+          salesavantScheduledJobs(
+            search: $search
+            first: $first
+            offset: $offset
+          ) {
             totalResults
             salesavantJobsList {
               modificationTime
@@ -75,45 +76,27 @@ export default {
             }
           }
         }
-      `;
-
-      this.$apollo
-        .query({
-          query: salesavantScheduledJobs,
-          variables: {
-            search: this.search.length >= 3 ? this.search : "",
-            first: this.itemsPerPage,
-            offset: this.itemsPerPage * this.page - this.itemsPerPage
-          },
-          fetchPolicy: "no-cache"
-        })
-        .then(resp => {
-          if (!!resp.data && !!resp.data.salesavantScheduledJobs) {
-            this.totalResults = resp.data.salesavantScheduledJobs.totalResults;
-            this.items = resp.data.salesavantScheduledJobs.salesavantJobsList;
-          }
-        });
-    },
-    onSearch: function() {
-      console.log("onSearch");
-      this.loadData();
+      `,
+      variables() {
+        return {
+          search: this.search.length >= 3 ? this.search : "",
+          first: this.itemsPerPage,
+          offset: this.itemsPerPage * this.page - this.itemsPerPage
+        };
+      },
+      skip() {
+        return this.search.length > 0 && this.search.length < 2;
+      }
     }
   },
-  props: {},
+  methods: {
+    updateOptions({ dataFromEvent: { page = 1, itemsPerPage = 10 } }) {
+      this.page = page;
+      this.itemsPerPage = itemsPerPage;
+    }
+  },
   components: {
     ScheduledJobsTable
-  },
-  mounted: function() {
-    this.loadData();
-    this.interval = setInterval(
-      function() {
-        this.loadData();
-      }.bind(this),
-      3000
-    );
-  },
-  beforeDestroy: function() {
-    clearInterval(this.interval);
   }
 };
 </script>
