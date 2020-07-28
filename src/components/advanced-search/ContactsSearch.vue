88330540d8df72b891ae3f1bddcd3dcc6175669f
Alejandro <lariverosc@gmail.com>
2020-07-28 14:10:05 -0500

    Refactor contact search view

--- src/components/advanced-search/ContactsSearch.vue
+++ src/components/advanced-search/ContactsSearch.vue
@@ -1,194 +1,194 @@
 <template>
-  <div>
-    <v-form ref="simpleSearchForm" @submit.prevent class="pt-2">
-      <v-container fluid>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="name"
-              v-model="contactSearch.name"
-              label="Name"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="title"
-              v-model="contactSearch.title"
-              label="Title"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="department"
-              label="Department"
-              v-model="contactSearch.department"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="rank"
-              v-model="contactSearch.rank"
-              label="Rank"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="city"
-              v-model="contactSearch.city"
-              label="City"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="state"
-              v-model="contactSearch.state"
-              label="State"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="region"
-              v-model="contactSearch.region"
-              label="Region"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="country"
-              v-model="contactSearch.country"
-              label="Country"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="moreThanScaleScoreAverage"
-              v-model="contactSearch.moreThanScaleScoreAverage"
-              label="More Than Than Scale Score Avg."
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="lessThanScaleScoreAverage"
-              v-model="contactSearch.lessThanScaleScoreAverage"
-              label="Less Than Scale Score Avg."
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="moreThanCesa"
-              v-model="contactSearch.moreThanCesa"
-              label="More Than Capital Efficiency Score Avg."
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="lessThanCesa"
-              v-model="contactSearch.lessThanCesa"
-              label="Less Than Capital Efficiency Score Avg."
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="moreThanWolfpackScore"
-              v-model="contactSearch.moreThanWolfpackScore"
-              label="More Than Wolfpack Score"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="lessThanWolfpackScore"
-              v-model="contactSearch.lessThanWolfpackScore"
-              label="Less Than Wolfpack Score"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row wrap>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="moreThanNumberOfExits"
-              v-model="contactSearch.moreThanNumberOfExits"
-              label="More Than Number Of Exits"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              :dense="denseForm"
-              name="lessThanNumberOfExits"
-              v-model="contactSearch.lessThanNumberOfExits"
-              label="Less Than Number Of Exits"
-              autocomplete="off"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-divider></v-divider>
-        <v-row wrap class="pt-2">
-          <v-col cols="12">
-            <playlists-autocomplete :dense="denseForm" playlistType="contact" @change="onPlaylistAutocompleteChange" />
-          </v-col>
-        </v-row>
-        <v-divider></v-divider>
-        <v-row wrap>
-          <v-col cols="12" md="6" class="d-flex justify-start align-center">
-            <a
-              @click.prevent="clearAll"
-              class="text-capitalize body-2"
-              block
-              small
-              color="primary"
-            >clear all</a>
-          </v-col>
-          <v-col cols="12" md="6" class="d-flex justify-center align-center">
-            <v-btn @click="search" class="text-capitalize" block type="submit" color="primary">
-              <v-icon small class="pr-1">search</v-icon>
-              {{"Search"}}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-  </div>
+  <v-form ref="simpleSearchForm" @submit.prevent>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="name"
+          v-model="contactSearch.name"
+          label="Name"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="title"
+          v-model="contactSearch.title"
+          label="Title"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="department"
+          label="Department"
+          v-model="contactSearch.department"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="rank"
+          v-model="contactSearch.rank"
+          label="Rank"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="city"
+          v-model="contactSearch.city"
+          label="City"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="state"
+          v-model="contactSearch.state"
+          label="State"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="region"
+          v-model="contactSearch.region"
+          label="Region"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="country"
+          v-model="contactSearch.country"
+          label="Country"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="moreThanScaleScoreAverage"
+          v-model="contactSearch.moreThanScaleScoreAverage"
+          label="More Than Than Scale Score Avg."
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="lessThanScaleScoreAverage"
+          v-model="contactSearch.lessThanScaleScoreAverage"
+          label="Less Than Scale Score Avg."
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="moreThanCesa"
+          v-model="contactSearch.moreThanCesa"
+          label="More Than Capital Efficiency Score Avg."
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="lessThanCesa"
+          v-model="contactSearch.lessThanCesa"
+          label="Less Than Capital Efficiency Score Avg."
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="moreThanWolfpackScore"
+          v-model="contactSearch.moreThanWolfpackScore"
+          label="More Than Wolfpack Score"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="lessThanWolfpackScore"
+          v-model="contactSearch.lessThanWolfpackScore"
+          label="Less Than Wolfpack Score"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row wrap>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="moreThanNumberOfExits"
+          v-model="contactSearch.moreThanNumberOfExits"
+          label="More Than Number Of Exits"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field
+          :dense="denseForm"
+          name="lessThanNumberOfExits"
+          v-model="contactSearch.lessThanNumberOfExits"
+          label="Less Than Number Of Exits"
+          autocomplete="off"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row wrap class="pt-2">
+      <v-col cols="12">
+        <playlists-autocomplete
+          :dense="denseForm"
+          playlistType="contact"
+          @change="onPlaylistAutocompleteChange"
+        />
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row wrap>
+      <v-col cols="12" md="6" class="d-flex justify-start align-center">
+        <a
+          @click.prevent="clearAll"
+          class="text-capitalize body-2"
+          block
+          small
+          color="primary"
+        >clear all</a>
+      </v-col>
+      <v-col cols="12" md="6" class="d-flex justify-center align-center">
+        <v-btn @click="search" class="text-capitalize" block type="submit" color="primary">
+          <v-icon small class="pr-1">search</v-icon>
+          {{"Search"}}
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
 <script>
@@ -205,8 +205,8 @@ export default {
       contactSearch: { ...defaultContactSearch },
       window: {
         width: 0,
-        height: 0
-      }
+        height: 0,
+      },
     };
   },
   methods: {
@@ -250,16 +250,16 @@ export default {
       this.contactSearch = {
         ...this.contactSearch,
         playlistUid: _get(value, "playlistUid", ""),
-        displayPlaylistUid: _get(value, "displayPlaylistUid", "")
+        displayPlaylistUid: _get(value, "displayPlaylistUid", ""),
       };
     },
     handleResize() {
       this.window.width = window.innerWidth;
       this.window.height = window.innerHeight;
-    }
+    },
   },
   components: {
-    PlaylistsAutocomplete
+    PlaylistsAutocomplete,
   },
   created() {
     window.addEventListener("resize", this.handleResize);
@@ -271,8 +271,8 @@ export default {
   computed: {
     denseForm() {
       return this.window.width > 1440 ? false : true;
-    }
-  }
+    },
+  },
 };
 </script>
 
