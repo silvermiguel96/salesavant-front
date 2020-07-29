8c9cb59db4fe6d9a202e1779e3bb5c2174ba1adb
Miguelangel <silvermiguel96@gmail.com>
2020-07-29 13:46:37 -0500

    Elevation in simple table

--- src/components/contacts/ContactsTable.vue
+++ src/components/contacts/ContactsTable.vue
@@ -68,44 +68,48 @@
     </template>
     <template v-slot:expanded-item="{ headers, item }">
       <td class="ma-0 pa-0" :colspan="headers.length">
-        <v-simple-table>
-          <template v-slot:default>
-            <tbody>
-              <tr v-for="job in item.otherJobs" :key="job.uid">
-                <td style="width:4.5%;"></td>
-                <td style="width:15%;">
-                  <div></div>
-                </td>
-                <td style="width:15%;">
-                  <div>
-                    <router-link :to="`/companies/${job.company.uid}`">{{ job.company.name || "" }}</router-link>
-                  </div>
-                </td>
-                <td style="width:15%;">
-                  <div>{{ job.title || "" }}</div>
-                </td> 
-                <td style="width:10%;">
-                  <div>{{ job.department || "--" }}</div>
-                </td>
-                <td style="width:10%;">
-                  <div>{{ job.rank || "--" }}</div>
-                </td>
-                <td style="width:6%;">
-                  <div>{{ job.company.scaleScore || "--" }}</div>
-                </td>
-                <td style="width:6%;">
-                  <div>{{ job.company.capitalEfficiencyScore ? job.company.capitalEfficiencyScore.toLocaleString() : "--" }}</div>
-                </td>
-                <td style="width:6%;">
-                  <div>--</div>
-                </td>
-                <td style="width:6%;">
-                  <div>--</div>
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
+        <v-card elevation="5">
+          <v-simple-table>
+            <template v-slot:default>
+              <tbody>
+                <tr v-for="job in item.otherJobs" :key="job.uid">
+                  <td style="width:4.5%;"></td>
+                  <td style="width:15%;">
+                    <div></div>
+                  </td>
+                  <td style="width:15%;">
+                    <div>
+                      <router-link
+                        :to="`/companies/${job.company.uid}`"
+                      >{{ job.company.name || "" }}</router-link>
+                    </div>
+                  </td>
+                  <td style="width:15%;">
+                    <div>{{ job.title || "" }}</div>
+                  </td>
+                  <td style="width:10%;">
+                    <div>{{ job.department || "--" }}</div>
+                  </td>
+                  <td style="width:10%;">
+                    <div>{{ job.rank || "--" }}</div>
+                  </td>
+                  <td style="width:6%;">
+                    <div>{{ job.company.scaleScore || "--" }}</div>
+                  </td>
+                  <td style="width:6%;">
+                    <div>{{ job.company.capitalEfficiencyScore ? job.company.capitalEfficiencyScore.toLocaleString() : "--" }}</div>
+                  </td>
+                  <td style="width:6%;">
+                    <div>--</div>
+                  </td>
+                  <td style="width:6%;">
+                    <div>--</div>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-card>
       </td>
     </template>
   </v-data-table>
@@ -121,107 +125,107 @@ export default {
           text: "Full Name",
           value: "fullName",
           width: "15%",
-          sortable: false
+          sortable: false,
         },
         {
           text: "Company",
           value: "companies.name",
           width: "15%",
-          sortable: false
+          sortable: false,
         },
         {
           text: "Title",
           value: "companies.title",
           width: "15%",
-          sortable: false
+          sortable: false,
         },
         {
           text: "Department",
           value: "companies.deparment",
           width: "10%",
-          sortable: false
+          sortable: false,
         },
         {
           text: "Rank",
           value: "companies.rank",
           width: "10%",
-          sortable: false
+          sortable: false,
         },
         {
           text: "Scale Score",
           value: "scaleScoreAverage",
           width: "6%",
           align: "left",
-          sortable: true
+          sortable: true,
         },
         {
           text: "Capital E.S.",
           value: "capitalEfficiencyScoreAverage",
           width: "6%",
           sortable: true,
-          align: "left"
+          align: "left",
         },
         {
           text: "Wolfpack Score",
           value: "wolfpackScore",
           width: "6%",
           sortable: true,
-          align: "left"
+          align: "left",
         },
         {
           text: "Exits",
           value: "numberOfExits",
           width: "6%",
           sortable: true,
-          align: "left"
-        }
+          align: "left",
+        },
       ],
       options: {
         page: 1,
-        itemsPerPage: 10
-      }
+        itemsPerPage: 10,
+      },
     };
   },
   methods: {
     updateOptions(dataFromEvent = {}) {
       this.$emit("updateOptions", { dataFromEvent });
-    }
+    },
   },
   computed: {
     formatedItems() {
-      return this.items.map(function(contact) {
-        let currentJobs = contact.companies.filter(function(job) {
+      return this.items.map(function (contact) {
+        let currentJobs = contact.companies.filter(function (job) {
           if (job.isCurrent) {
             return job;
           }
         });
-        if (currentJobs.length > 0){
+        if (currentJobs.length > 0) {
           contact.currentOrLastJobs = currentJobs;
         }
-        let otherJobs = contact.companies.filter(function(job) {
+        let otherJobs = contact.companies.filter(function (job) {
           if (!job.isCurrent) {
             return job;
           }
         });
-        if (contact.companies.length >= 1){
-          if (currentJobs.length == 0){
+        if (contact.companies.length >= 1) {
+          if (currentJobs.length == 0) {
             contact.currentOrLastJobs = [contact.companies[0]];
-            contact.otherJobs =  contact.companies.slice(1);
-          }else{
+            contact.otherJobs = contact.companies.slice(1);
+          } else {
             contact.otherJobs = otherJobs;
           }
-        }else{
+        } else {
           contact.currentOrLastJobs = [];
-          contact.otherJobs = [];  
+          contact.otherJobs = [];
         }
         return contact;
       });
-    }
+    },
   },
   props: {
     items: Array,
-    totalResults: Number
-  }
+    totalResults: Number,
+  },
 };
 </script>
 
