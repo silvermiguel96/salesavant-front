d046cc9b6cd235e89084b217fbbf552337b1d4ae
Miguelangel <silvermiguel96@gmail.com>
2020-08-04 16:37:31 -0500

    Requiered and icons

--- src/components/account/UsersTable.vue
+++ src/components/account/UsersTable.vue
@@ -1,41 +1,60 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    :server-items-length="totalResults"
-    :items-per-page="options.itemsPerPage"
-    :footer-props="{
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :server-items-length="totalResults"
+      :items-per-page="options.itemsPerPage"
+      :footer-props="{
       'items-per-page-options': [10, 20, 50]
     }"
-    :options.sync="options"
-    class="mx-2"
-    @update:options="updateOptions"
-  >
-    <template v-slot:item="{ item }">
-      <tr>
-        <td>{{ item.firstName }} {{ item.lastName}}</td>
-        <td>{{ item.email }}</td>
-        <td class="text-capitalize">{{ item.status}}</td>
-        <td>
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <div class="d-flex align-center justify-center">
-                <v-icon
-                  @click="removePlaylist(item)"
-                  color="deep-orange lighten-2"
-                  size="20"
-                  small
-                  v-bind="attrs"
-                  v-on="on"
-                >person</v-icon>
-              </div>
-            </template>
-            <span>Inactive User</span>
-          </v-tooltip>
-        </td>
-      </tr>
-    </template>
-  </v-data-table>
+      :options.sync="options"
+      class="mx-2"
+      @update:options="updateOptions"
+    >
+      <template v-slot:item="{ item }">
+        <tr>
+          <td>{{ item.fullName }}</td>
+          <td>{{ item.email }}</td>
+          <td class="text-capitalize">{{ item.status}}</td>
+          <td v-if="item.status === 'active'">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <div class="d-flex align-center justify-center">
+                  <v-icon
+                    @click="deactivateUser(item)"
+                    color="primary"
+                    size="20"
+                    small
+                    v-bind="attrs"
+                    v-on="on"
+                  >person</v-icon>
+                </div>
+              </template>
+              <span>Deactivate user</span>
+            </v-tooltip>
+          </td>
+          <td v-if="item.status === 'inactive'">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <div class="d-flex align-center justify-center">
+                  <v-icon
+                    @click="activeUser(item)"
+                    color="primary"
+                    size="20"
+                    small
+                    v-bind="attrs"
+                    v-on="on"
+                  >person_outline</v-icon>
+                </div>
+              </template>
+              <span>Active user</span>
+            </v-tooltip>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -50,50 +69,80 @@ export default {
           value: "numEmployees",
           width: "15%",
           sortable: true,
-          divider: true
+          divider: true,
         },
         {
           text: "Action",
           align: "center",
           value: "state",
           width: "15%",
-          sortable: false
-        }
+          sortable: false,
+        },
       ],
       options: {
         page: 1,
-        itemsPerPage: 10
-      }
+        itemsPerPage: 10,
+      },
     };
+  },
+  computed: {
+    users() {
+      return this.items.map((item) => {
+        if (item.firstName || item.lastName) {
+          return { ...item, fullName: `${item.firstName} ${item.lastName}` };
+        } else {
+          return { ...item, fullName: "--" };
+        }
+      });
+    },
   },
   methods: {
     updateOptions(dataFromEvent = {}) {
       this.$emit("updateOptions", { dataFromEvent });
     },
-    async removePlaylist(item) {
+    async deactivateUser(item) {
       const res = await this.$confirm(
-        ` <h1 class="subtitle-1">Confirm you want to Inactive User 
-        <span class="font-weight-bold">${item.firstName} ${item.lastName}</span>?</h1>`,
+        ` <h1 class="subtitle-1">Confirm you want to deactivate User 
+        <span class="font-weight-bold">${item.email}</span>?</h1>`,
         {
-          buttonTrueText: "Inactive",
+          buttonTrueText: "Deactivate",
           buttonFalseText: "close",
           buttonTrueColor: "red lighten-2",
-          color: "deep-orange lighten-2",
+          color: "primary",
           icon: "warning",
-          title: "Inactive User",
-          width: 600
+          title: "Deactivate User",
+          width: 600,
         }
       );
       if (res) {
         console.log("the playlist", item);
         this.$emit("deletePlaylist", item);
       }
-    }
+    },
+    async activeUser(item) {
+      const res = await this.$confirm(
+        ` <h1 class="subtitle-1">Confirm you want to actitve User 
+        <span class="font-weight-bold">${item.email}</span>?</h1>`,
+        {
+          buttonTrueText: "Active",
+          buttonFalseText: "close",
+          buttonTrueColor: "red lighten-2",
+          color: "primary",
+          icon: "warning",
+          title: "Actitve User",
+          width: 600,
+        }
+      );
+      if (res) {
+        console.log("the playlist", item);
+        this.$emit("deletePlaylist", item);
+      }
+    },
   },
   props: {
     items: Array,
-    totalResults: Number
-  }
+    totalResults: Number,
+  },
 };
 </script>
 
--- src/components/admin/company/CompanyProfile.vue
+++ src/components/admin/company/CompanyProfile.vue
@@ -1,160 +1,100 @@
 <template>
   <v-card v-if="company" style="height:100%;" class="pa-3">
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Website Admin" color="primary" v-model="company.url"></v-text-field>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-row no-gutters>
+        <v-col cols="12" md="6">
+          <v-text-field
+            dense
+            :rules="websiteRules"
+            label="Website Admin"
+            color="primary"
+            v-model="company.url"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Momentum Admin" color="primary" v-model="company.momentum"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Status" color="primary" v-model="company.status"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            dense
+            label="Name"
+            :rules="nameRules"
+            color="primary"
+            required
+            v-model="company.name"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Vertical Groups" color="primary" v-model="company.vertical"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Industry" color="primary" v-model="company.industry"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Country" color="primary" v-model="company.country"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="State" color="primary" v-model="company.state"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="City" color="primary" v-model="company.city"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Employees" color="primary" v-model="company.numEmployees"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Scale Score" color="primary" v-model="company.scaleScore"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            dense
+            label="Capital Efficiency Score"
+            color="primary"
+            v-model="company.capitalEfficiencyScore"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Acquirer Name" color="primary" v-model="company.acquirerName"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Date Founded" color="primary" v-model="company.dateFounded"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Exit Date" color="primary" v-model="company.exitDate"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense label="Exit Value" color="primary" v-model="company.exitValue"></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-textarea class="pa-0 ma-0" v-model="company.description" label="Description"></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row no-gutters class="d-flex justify-end">
+        <v-col cols="12" md="6">
+          <v-row class="d-flex" no-gutters>
+            <v-col cols="12" md="6" class="px-1">
+              <v-btn block class="text-capitalize" dark color="red lighten-1">
+                <v-icon small>delete</v-icon>Delete
+              </v-btn>
+            </v-col>
+            <v-col cols="12" md="6" class="px-1">
+              <v-btn
+                block
+                class="text-capitalize"
+                :disabled="!valid"
+                @click="validate"
+                color="primary"
+              >
+                <v-icon small>save</v-icon>Save
+              </v-btn>
+            </v-col>
           </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Momentum Admin" color="primary" v-model="company.momentum"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Vertical Groups" color="primary" v-model="company.vertical"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Industry" color="primary" v-model="company.industry"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Country" color="primary" v-model="company.country"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="State" color="primary" v-model="company.state"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="City" color="primary" v-model="company.city"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Employees" color="primary" v-model="company.numEmployees"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Scale Score" color="primary" v-model="company.scaleScore"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field
-              dense
-              label="Capital Efficiency Score"
-              color="primary"
-              v-model="company.capitalEfficiencyScore"
-            ></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field
-              dense
-              label="Acquirer Name"
-              color="primary"
-              v-model="company.acquirerName"
-            ></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Date Founded" color="primary" v-model="company.dateFounded"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Exit Date" color="primary" v-model="company.exitDate"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Exit Value" color="primary" v-model="company.exitValue"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <v-row class="d-md-block" no-gutters>
-            <v-text-field dense label="Status" color="primary" v-model="company.status"></v-text-field>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="12">
-        <v-card-text>
-          <v-row no-gutters>
-            <v-textarea v-model="company.description" label="Description"></v-textarea>
-          </v-row>
-        </v-card-text>
-      </v-col>
-    </v-row>
-    <v-row no-gutters class="d-flex justify-end">
-      <v-col cols="12" md="6">
-        <v-row class="d-flex" no-gutters>
-          <v-col cols="12" md="6" class="px-1">
-            <v-btn block class="text-capitalize" dark color="red lighten-1">
-              <v-icon small>delete</v-icon>Delete
-            </v-btn>
-          </v-col>
-          <v-col cols="12" md="6" class="px-1">
-            <v-btn block class="text-capitalize" color="primary">
-              <v-icon small>save</v-icon>Save
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+        </v-col>
+      </v-row>
+    </v-form>
   </v-card>
 </template>
 
@@ -166,6 +106,15 @@ export default {
   data() {
     return {
       company: null,
+      valid: true,
+      websiteRules: [
+        (v) => !!v || "Url is required",
+        (v) =>
+          /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/.test(
+            v
+          ) || "Url is not valid",
+      ],
+      nameRules: [(v) => !!v || "Name is required"],
     };
   },
   components: {
@@ -221,6 +170,11 @@ export default {
         : `http://${this.company.url}`;
     },
   },
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+  },
 };
 </script>
 
@@ -228,5 +182,11 @@ export default {
 .v-card__text {
   padding: 0.5em;
   padding-left: 1em;
+}
+.col-6 {
+  padding: 0px 5px !important;
+}
+.col-12 {
+  padding: 3px 6px !important;
 }
 </style>
