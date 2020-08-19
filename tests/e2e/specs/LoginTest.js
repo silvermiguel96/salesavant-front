1475c229b01acbef13f339e5ce5b9ae939c5a4f4
silvermiguel96 <silvermiguel96@gmail.com>
2020-08-19 15:24:19 -0500

    new test

--- tests/e2e/specs/LoginTest.js
+++ tests/e2e/specs/LoginTest.js
@@ -1,27 +1,10 @@
-// https://docs.cypress.io/api/introduction/api.html
 
 describe("Login Test", () => {
   it("Visits the app root url", () => {
-    cy.visit("http://localhost:8080/");
-
-    cy.get("#input-19")
-      .type("alejandro@salesavant.com")
-      .should("have.value", "alejandro@salesavant.com");
-
-    cy.get("#input-23")
-      .type("qweqwe")
-      .should("have.value", "qweqwe");
-
-    cy.get(".btn-primary")
-      .click();
-
-    cy.url().should('include', '/home')
-
-    cy.get('.Account').click();
-
-    cy.get('.mainMenu').contains('Logout').click()
-
-    cy.url().should('include', '/')
-
+    cy.visit("http://localhost:8080/home");
+    cy.get("#input-19").type("alejandro@salesavant.com");
+    cy.get("#input-23").type("qweqwe");
+    cy.get(".btn-primary").click();
+    cy.url().should('include', '/home');
   });
 });
