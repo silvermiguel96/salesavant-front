de6b6ae018187a2ed6d03a20dfc8241f3240845c
silvermiguel96 <silvermiguel96@gmail.com>
2020-08-19 16:50:37 -0500

    Review Code

--- tests/e2e/specs/LoginTest.js
+++ tests/e2e/specs/LoginTest.js
@@ -4,7 +4,7 @@ describe("Login Test", () => {
     cy.visit("http://localhost:8080/home");
     cy.get("#input-19").type("alejandro@salesavant.com");
     cy.get("#input-23").type("qweqwe");
-    cy.get(".btn-primary").click();
+    cy.get("#btn-primary").click();
     cy.url().should('include', '/home');
   });
 });
