3ca4bd408a05d7992f397cef322b2c6a38907339
Alejandro <lariverosc@gmail.com>
2020-09-17 12:43:22 -0500

    Move total comapnies to sigbnal

--- src/components/dashboard/CustomSalesSignals.vue
+++ src/components/dashboard/CustomSalesSignals.vue
@@ -1,32 +1,27 @@
 <template>
-  <!-- Custom Sales Signals -->
   <v-card-text>
     <v-toolbar flat color="white">
-      <v-toolbar-title>Custom Sales Signals</v-toolbar-title>
+      <v-toolbar-title>Custom Signals</v-toolbar-title>
     </v-toolbar>
-    <v-row no-gutters v-if="companySignalsAggs">
+    <v-row v-if="signals" no-gutters>
       <v-col cols="12">
         <v-data-table
           :headers="headers"
-          :items="companySignalsAggs.companySignalAggsList"
-          :server-items-length="companySignalsAggs.totalResults"
+          :items="signals.signalsList"
+          :server-items-length="signals.totalResults"
           :items-per-page="options.itemsPerPage"
-          :sort-by="['totalCompanies']"
-          :sort-desc="[true]"
           :options.sync="options"
+          @update:options="updateOptions"
           class="mx-2"
-          @updateOptions="updateOptions"
         >
           <template v-slot:item="{ item }">
             <tr>
               <td>
-                <router-link
-                  :to="`/signals/${item.signal.id}`"
-                >{{ item.signal.name || item.signal.description }}</router-link>
+                <router-link :to="`/signals/${item.id}`">{{ item.name || item.description }}</router-link>
               </td>
               <td>{{item.totalCompanies.toLocaleString() || "0"}}</td>
-              <td>{{item.signal.score.toLocaleString() || "0"}}</td>
-              <td>{{item.signal.group || "-"}}</td>
+              <td>{{item.score.toLocaleString() || "0"}}</td>
+              <td>{{item.group || "-"}}</td>
             </tr>
           </template>
         </v-data-table>
@@ -34,14 +29,7 @@
     </v-row>
     <v-row justify="center" no-gutters>
       <v-col cols="12">
-        <v-progress-linear
-          :active="!!isLoading"
-          color="blue"
-          indeterminate
-          absolute
-          bottom
-          query
-        ></v-progress-linear>
+        <v-progress-linear :active="!!isLoading" color="blue" indeterminate absolute bottom query></v-progress-linear>
       </v-col>
     </v-row>
   </v-card-text>
@@ -52,52 +40,61 @@ export default {
   data() {
     return {
       isLoading: true,
-      companySignalsAggs: null,
       headers: [
         {
           text: "Custom Signal",
           value: "customsignal",
           sortable: false,
-          width: "20%"
+          width: "20%",
         },
         {
           text: "Count",
           value: "totalCompanies",
           sortable: true,
-          width: "20%"
+          width: "20%",
         },
-        { text: "Score", value: "score", sortable: false, width: "20%" },
+        { text: "Score", value: "score", sortable: true, width: "20%" },
         {
           text: "Group",
           align: "left",
           value: "group",
           sortable: false,
-          width: "20%"
-        }
+          width: "20%",
+        },
       ],
-      descending: false,
+      signals: [],
       options: {
         page: 1,
-        itemsPerPage: 10
+        itemsPerPage: 10,
+        sortBy: [],
+        sortDesc:[],
+        apiSortBy: "",
+        apiSortOrder: "",
       },
-      sortBy: "",
-      isFiltered: false
     };
   },
   apollo: {
-    companySignalsAggs: {
+    signals: {
       query: gql`
-        query companySignalsAggs($first: Int, $offset: Int) {
-          companySignalsAggs(first: $first, offset: $offset) {
+        query signals(
+          $first: Int
+          $offset: Int
+          $sortBy: String
+          $sortOrder: String
+        ) {
+          signals(
+            first: $first
+            offset: $offset
+            sortBy: $sortBy
+            sortOrder: $sortOrder
+          ) {
             totalResults
-            companySignalAggsList {
-              signal {
-                id
-                name
-                description
-                score
-                group
-              }
+            signalsList {
+              id
+              name
+              description
+              score
+              group
               totalCompanies
             }
           }
@@ -108,23 +105,37 @@ export default {
           first: this.options.itemsPerPage,
           offset:
             this.options.itemsPerPage * this.options.page -
-            this.options.itemsPerPage
+            this.options.itemsPerPage,
+          sortBy: this.options.apiSortBy,
+          sortOrder: this.options.apiSortOrder,
         };
       },
       watchLoading(isLoading, countModifier) {
         this.isLoading = isLoading;
       },
-      fetchPolicy: "cache-and-network"
-    }
+      fetchPolicy: "cache-and-network",
+    },
   },
   methods: {
-    updateOptions({
-      dataFromEvent: { page = 1, itemsPerPage = 10, sortBy = [], sortDesc = [] }
-    }) {
-      this.options.page = options.page;
-      this.options.itemsPerPage = options.itemsPerPage;
-    }
-  }
+    updateOptions({ page = 1, itemsPerPage = 10, sortBy = [], sortDesc = [] }) {
+      this.options.page = page;
+      this.options.itemsPerPage = itemsPerPage;
+      if (sortBy.length > 0) {
+        this.options.apiSortBy = sortBy[0];
+      } else {
+        this.options.apiSortBy = "";
+      }
+      if (sortDesc.length > 0) {
+        if (sortDesc[0]) {
+          this.options.apiSortOrder = "desc";
+        } else {
+          this.options.apiSortOrder = "asc";
+        }
+      } else {
+        this.options.apiSortOrder = "";
+      }
+    },
+  },
 };
 </script>
 
