4d8452a125044bf0eaa93d6f4470d00d2c66a332
Alejandro <lariverosc@gmail.com>
2020-09-18 00:57:09 -0500

    Fix session management use store

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
 import Vue from "vue";
 import vuetify from "./plugins/vuetify";
-import VuetifyConfirm from 'vuetify-confirm'
-import App from "./App.vue";
+import VuetifyConfirm from "vuetify-confirm";
 import router from "./router";
+import store from "./store";
 import { createProvider } from "./vue-apollo";
 import VueApexCharts from "vue-apexcharts";
-import store from "./store";
+import App from "./App.vue";
 
-Vue.component('apexchart', VueApexCharts)
 Vue.config.productionTip = false;
+Vue.prototype.$eventBus = new Vue();
 
 Vue.use(VuetifyConfirm, { vuetify })
-Vue.use(require('vue-moment'));
-
-
-Vue.prototype.$eventBus = new Vue();
+Vue.use(require("vue-moment"));
+Vue.component("apexchart", VueApexCharts)
 
 new Vue({
   vuetify,
